d0585aa76fe2bf23e1ec8fb5049cca909aa61394
jdkoreclipse <86:1b:3f:a6:da:d5:0d:36:59:34:0d:a0:44:61:90:f9>
2011-05-09 17:15:35 -0400

    fix errors

--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "2.6.35.9-imoseyon-0.8"
+#define UTS_RELEASE "2.6.35.13-jdkernel-g1f56ea5-dirty"
---
+++ usr/.initramfs_data.cpio.d
@@ -0,0 +1,1 @@
+deps_initramfs := /home/jdkoreclipse/leankernel/jdkernel/scripts/gen_initramfs_list.sh \
