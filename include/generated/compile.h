e9faea80ee19a18e6c9d0183e8213efb7f810640
jdkoreclipse <86:1b:3f:a6:da:d5:0d:36:59:34:0d:a0:44:61:90:f9>
2011-05-09 15:54:42 -0400

    import 2.6.35.9 source ported from the Vivo (thx imoseyon!)....this has all the mecha board files included

---
+++ include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 3 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#3 PREEMPT Mon Apr 11 17:00:52 PDT 2011"
+#define LINUX_COMPILE_TIME "17:00:52"
+#define LINUX_COMPILE_BY "simon"
+#define LINUX_COMPILE_HOST "ubuntu"
+#define LINUX_COMPILE_DOMAIN "corp.meebo.com"
+#define LINUX_COMPILER "gcc version 4.5.1 (Sourcery G++ Lite 2010.09-50) "
---
+++ include/generated/utsrelease.h
@@ -0,0 +1,1 @@
+#define UTS_RELEASE "2.6.35.9-imoseyon-0.8"
